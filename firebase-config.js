d1d5e6f1498119b3627a01f0bc0a98010a3aa695
Jarosław <jarekderda@gmail.com>
2025-08-08 12:50:01 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,14 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Twoja konfiguracja z konsoli Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyCKCF4Csccas02cPF5ebuQ17dFiFOHMCV8",
   authDomain: "opowiadania-derda.firebaseapp.com",
   projectId: "opowiadania-derda",
-  storageBucket: "opowiadania-derda.firebasestorage.app",
+  storageBucket: "opowiadania-derda.appspot.com",
   messagingSenderId: "82484134326",
   appId: "1:82484134326:web:8b702517a5619c6ef94117",
   measurementId: "G-HSKHJ6FGR5"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+// Inicjalizacja Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore(); // Inicjalizacja Firestore
